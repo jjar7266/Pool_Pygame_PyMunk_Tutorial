490adf17921a4f2fe189c4fb89a35393b56cca59
jjar7266 <jjar7266@gmail.com>
2026-08-19 02:32:08 -0400

    Save point: Completed full CodingWithRuss follow-along version

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ pygame.init()
 # Setup game window
 SCREEN_WIDTH  = 1200
 SCREEN_HEIGHT = 678
+BOTTOM_PANEL  = 50
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT + BOTTOM_PANEL))
 pygame.display.set_caption("Pool made with Pygame/Pymunk")
 
 # PyMunk Space
@@ -28,7 +29,27 @@ clock = pygame.time.Clock()
 FPS = 120
 
 # Game Variables
-dia = 36
+lives           = 3
+dia             = 36
+pocket_dia      = 66
+force           = 0
+max_force       = 10000
+force_direction = 1
+game_running    = True
+cue_ball_potted = False
+taking_shot     = True
+powering_up     = False
+
+potted_balls = []
+
+# COLORs (RGB)
+BG    = ( 50,  50,  50)
+RED   = (255,   0,   0)
+WHITE = (255, 255, 255)
+
+# Fonts
+font       = pygame.font.SysFont("Lato", 30)
+large_font = pygame.font.SysFont("Lato", 60)
 
 # Load Images
 cue_image = pygame.image.load("assets/images/cue.png").convert_alpha()
@@ -39,9 +60,12 @@ for i in range(1, 17):
     ball_image = pygame.image.load(f"assets/images/ball_{i}.png").convert_alpha()
     ball_images.append(ball_image)
 
-# COLORs (RGB)
-BG  = ( 50,  50,  50)
-RED = (255,   0,   0)
+
+# Function for outputting text onto the screen
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
 
 # Function for creating balls
 def create_ball(radius, pos):
@@ -76,6 +100,16 @@ for col in range(5):
 pos = (888, SCREEN_HEIGHT / 2)
 cue_ball = create_ball(dia / 2, pos)
 balls.append(cue_ball)
+
+# Create six pockets on table
+pockets = [
+    (55, 63),
+    (592, 48),
+    (1134, 64),
+    (55, 616),
+    (592, 629),
+    (1134, 616)
+]
 
 # Create pool table cushions
 cushions = [
@@ -115,10 +149,17 @@ class Cue():
 
     def draw(self, surface):
         self.image = pygame.transform.rotate(self.original_image, self.angle)
-        surface.blit(self.image,)
+        surface.blit(self.image,
+                    (self.rect.centerx - self.image.get_width() / 2,
+                    self.rect.centery - self.image.get_height() / 2)
+        )
 
 
 cue = Cue(balls[-1].body.position)
+
+# Create power bars to show how hard the cue ball will be hit.
+power_bar = pygame.Surface((10, 20))
+power_bar.fill(RED)
 
 # Game Loop
 run = True
@@ -134,28 +175,106 @@ while run:
     # Draw Pool Table
     screen.blit(table_image, (0, 0))
 
+    # Check if any balls have been potted
+    for i, ball in enumerate(balls):
+        for pocket in pockets:
+            ball_x_dist = abs(ball.body.position[0] - pocket[0])
+            ball_y_dist = abs(ball.body.position[1] - pocket[1])
+            ball_dist = math.sqrt((ball_x_dist ** 2) + (ball_y_dist ** 2))
+            if ball_dist <= pocket_dia / 2:
+
+                # check if the potted ball was the cue ball
+                if i == len(balls) - 1:
+                    lives -= 1
+                    cue_ball_potted = True
+                    ball.body.position = (-100, -100)
+                    ball.body.velocity = (0.0, 0.0)
+                else:
+                    space.remove(ball.body)
+                    balls.remove(ball)
+                    potted_balls.append(ball_images[i])
+                    ball_images.pop(i)
+
+
     # Draw Pool Balls
     for i, ball in enumerate(balls):
         screen.blit(ball_images[i], (ball.body.position[0] - ball.radius, ball.body.position[1] - ball.radius))
 
+    # Check if all the balls have stopped moving
+    taking_shot = True
+    for ball in balls:
+        if int(ball.body.velocity[0]) != 0 or int(ball.body.velocity[1]) != 0:
+            taking_shot = False
+
+
     # Draw Pool Cue
-    # calculate pool cue angle
-    mouse_pos = pygame.mouse.get_pos()
-    x_dist = balls[-1].body.position[0] - mouse_pos[0]
-    y_dist = -(balls[-1].body.position[1] - mouse_pos[1])  # negative because y coordinates increase down the screen
-    cue_angle = math.degrees(math.atan2(y_dist, x_dist))
-    cue.update(cue_angle)
+    if taking_shot == True and game_running == True:
+        if cue_ball_potted == True:
+            # reposition cue ball
+            balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
+            cue_ball_potted = False
 
-    cue.draw(screen)
+        # calculate pool cue angle
+        mouse_pos = pygame.mouse.get_pos()
+        cue.rect.center = balls[-1].body.position
+        x_dist = balls[-1].body.position[0] - mouse_pos[0]
+        y_dist = -(balls[-1].body.position[1] - mouse_pos[1])  # negative because y coordinates increase down the screen
+        cue_angle = math.degrees(math.atan2(y_dist, x_dist))
+        cue.update(cue_angle)
+        cue.draw(screen)
 
+
+    # Power up pool cue
+    if powering_up == True and game_running == True:
+        force += 100 * force_direction
+        if force >= max_force or force <= 0:
+            force_direction *= -1
+
+        # Draw power bars
+        for b in range(math.ceil(force / 2000)):
+            screen.blit(power_bar,
+                       (balls[-1].body.position[0] -30 + (b * 15),
+                        balls[-1].body.position[1] + 30))
+
+    # increase force variable
+    elif powering_up == False and taking_shot == True:
+        x_impulse = math.cos(math.radians(cue_angle))
+        y_impulse = math.sin(math.radians(cue_angle))
+        balls[-1].body.apply_impulse_at_local_point((force * -x_impulse, force * y_impulse), (0, 0))
+        force = 0
+        force_direction = 1
+
+    # draw bottom panel
+    pygame.draw.rect(screen, BG, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
+    draw_text("LIVES: " + str(lives), font, WHITE, SCREEN_WIDTH - 200, SCREEN_HEIGHT + 10)
+
+    # draw potted balls in bottom panel
+    for i, ball in enumerate(potted_balls):
+        screen.blit(ball, (10 + (i * 50), SCREEN_HEIGHT + 10))
+
+
+    # check for game over
+    if lives <= 0:
+        draw_text("GAMER OVER", large_font, WHITE, SCREEN_WIDTH / 2 - 160,
+                 SCREEN_HEIGHT / 2 - 100)
+        game_running = False
+
+    # check if all balls are potted
+    if len(balls) == 1:
+        draw_text("YOU WIN!!", large_font, WHITE, SCREEN_WIDTH / 2 - 160,
+                 SCREEN_HEIGHT / 2 - 100)
+        game_running = False
 
     # Event handler
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and taking_shot == True:
+            powering_up = True
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            cue_ball.body.apply_impulse_at_local_point((-1500, 0), (0, 0))
+        if event.type == pygame.MOUSEBUTTONUP and taking_shot == True:
+            powering_up = False
+
+        if event.type == pygame.QUIT:
+                    run = False
 
 
     #space.debug_draw(draw_options)
